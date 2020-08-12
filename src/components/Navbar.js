377ccf405fdc1191f5133e2107541f549f3bb67b
adiakbar <adi.akbar91@gmail.com>
2020-08-12 22:38:51 +0700

    contoh multiple context in class component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,3 +19,37 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+/*
+=== Cara menggunakan multiple context in class component ===
+
+export default class Navbar extends Component {
+  render() {
+    return(
+      <AuthContext.Consumer>{(AuthContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const {isLightTheme, light, dark} = themeContext
+          const {isAuthenticated, toggleAuth} = AuthContext
+          const theme = isLightTheme ? light : dark
+          return(
+            <nav style={{background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged In' : 'Logged Out'}
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          )
+        }}
+        </ThemeContext.Consumer>
+      )}
+      </AuthContext.Consumer>
+    )
+  }
+}
+*/
