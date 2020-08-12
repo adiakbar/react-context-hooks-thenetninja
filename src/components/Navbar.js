6bd33e07d53128b83a5bd496f7eb86eb560eb495
adiakbar <adi.akbar91@gmail.com>
2020-08-12 21:59:17 +0700

    inisiasi theme context

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
